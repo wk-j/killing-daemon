ba9c1e4f6ebd4f2e3d5a7665c9fba51c76b7e57b
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-08-04 20:15:06 +0700

    Initial commit

---
+++ Source/KillingDaemon/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Diagnostics
+open System.Runtime.Loader
+
+AssemblyLoadContext.Default.add_Unloading(fun c -> Console.WriteLine("Unload"))
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    while Console.ReadLine() <> "q" do
+        printfn ">> "
+    0 // return an integer exit code
